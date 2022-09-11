f75d148fab49be7ad8e57a61684a963853e534a6
Nazir.Ismailov <Nazir.Ismailov@x5.ru>
2022-09-11 14:50:32 +0500

    added pom and structure

---
+++ src/main/java/coreJdbc/JDBCTemplate.java
@@ -0,0 +1,29 @@
+package coreJdbc;
+
+import org.postgresql.ds.PGConnectionPoolDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCTemplate {
+    PGConnectionPoolDataSource ds = new PGConnectionPoolDataSource();
+    Connection connection = null;
+
+    private void setUp() {
+        ds.setServerNames(new String[]{"localhost"});
+        ds.setDatabaseName("hibe");
+        ds.setUser("postgres");
+        ds.setPassword("password");
+    }
+
+    public Connection getConnect() {
+        try {
+            setUp();
+            connection = ds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
